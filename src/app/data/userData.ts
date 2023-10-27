a3584ed813d51e061463f6d71f1dda8226a9060d
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2023-10-26 20:37:44 -0500

    feat(signUp/login): added user model and user data/updated login and sign up pages

---
+++ src/app/data/userData.ts
@@ -0,0 +1,41 @@
+import { User } from 'src/models/User';
+
+const appUsers: User[] = [
+  {
+    id: '1',
+    firstName: 'Emeric',
+    lastName: 'Kongla',
+    email: 'emericKongla@gmail.com',
+    password: 'uuduuduc88',
+  },
+  {
+    id: '2',
+    firstName: 'Alice',
+    lastName: 'Smith',
+    email: 'aliceSmith@gmail.com',
+    password: 'alice1234',
+  },
+  {
+    id: '3',
+    firstName: 'Bob',
+    lastName: 'Johnson',
+    email: 'bobJohnson@gmail.com',
+    password: 'bob1234',
+  },
+  {
+    id: '4',
+    firstName: 'Charlie',
+    lastName: 'Brown',
+    email: 'charlieBrown@gmail.com',
+    password: 'charlie1234',
+  },
+  {
+    id: '5',
+    firstName: 'David',
+    lastName: 'Williams',
+    email: 'davidWilliams@gmail.com',
+    password: 'david1234',
+  },
+];
+
+export default appUsers;
